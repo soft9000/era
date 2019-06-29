b936256c7369ee8385ada083405e7b85a61f9bc4
Soft9000.com <r.a.nagy@gmail.com>
2019-06-29 07:55:52 -0400

    Minor update.
    A little PEPy ...

--- ezlog.py
+++ ezlog.py
@@ -8,14 +8,31 @@ from email import utils
 
 class EzLog():
 
+    def __init__(self, message = ''):
+        """ Taking care of beez-nice """
+        self.LFORMAT = '%Y/%m/%d: %H.%M.%S (LOCAL)'
+        self.UFORMAT = '%Y/%m/%d: %H.%M.%S [%z]'
+        self._hack(message)
+
     def _hack(self, message):
         znow = zdatetime.now()
         znow = utils.localtime(znow)
         self.local_date = znow.strftime(format=self.LFORMAT)
         self.message = str(message)
 
+    def __str__(self):
+        return self.local_date + "\t" + self.message + "\n"
+
+    def hack(self, message):
+        """ Update the time, as well as the message """
+        self._hack(message)
+
+    def is_null(self):
+        return len(self.message) == 0
+
     @classmethod
     def Create(recipe):
+        ''' Extract the log message from the command line, if present.'''
         import sys
         words = sys.argv[1:]
         message = ''
@@ -25,29 +42,11 @@ class EzLog():
             message += word
         return recipe(message)
 
-    def __init__(self, message):
-        """ Taking care of beez-nice """
-        self.LFORMAT = '%Y/%m/%d: %H.%M.%S (LOCAL)'
-        self.UFORMAT = '%Y/%m/%d: %H.%M.%S [%z]'
-        self._hack(message)
-
-    def __repr__(self):
-        return self.local_date + "\t" + self.message + "\n"
-
-    def __str__(self):
-        return repr(self)
-
-    def hack(self, message):
-        """ Update the time, as well as the message """
-        self._hack(message)
-
-    def isNull(self):
-        return len(self.message) == 0
-
 
 if __name__ == '__main__':
     entry = EzLog.Create()
     with open("./logger.log", "a") as fp:
-        if entry.isNull():
+        if entry.is_null():
             entry.message = "This is a test"
         fp.write(str(entry))
+

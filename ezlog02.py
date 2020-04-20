5e2083e184bff694d797a5e23dfbb0e391d87f79
Randall Nagy <randall.nagy@gmail.com>
2020-04-20 14:10:26 -0400

    EZU_2000_EntryList
    Class discussion.

--- ezlog02.py
+++ ezlog02.py
@@ -4,7 +4,7 @@ Mission: Create + append a console-line message to 'logger.log' in the pwd.
 Project: Python 4000, Linux & DevOps (Udemy)
 URL:     https://www.udemy.com/course/python-4000-gnu-devops
 File:    ezlog.py
-Version: 2.0 (command-line injection test case)
+Version: 3.0 (list/read update & test case)
 """
 import os
 import os.path
@@ -12,6 +12,8 @@ from datetime import datetime as zdatetime
 from email import utils
 
 class EzLog():
+
+    LOGFILE = "./logger.log"
 
     def __init__(self, message = ''):
         self.LFORMAT = '%Y/%m/%d: %H.%M.%S (LOCAL)'
@@ -37,20 +39,48 @@ class EzLog():
     @staticmethod
     def Create(message):
         entry = EzLog(message)
-        with open("./logger.log", "a") as fp:
+        with open(EzLog.LOGFILE, "a") as fp:
             if entry.is_null():
                 entry.message = "This is a test"
             fp.write(str(entry))
         print("Logged.")
 
+    @staticmethod
+    def List(message):
+        try:
+            nelem = int(message)
+            if nelem < 1:
+                print(f"Ignoring {nelem}...")
+                return
+            with open(EzLog.LOGFILE) as fh:
+                for which in range(nelem):
+                    line = fh.readline()
+                    if not line:
+                        print("(eof)")
+                        return
+                    else:
+                        print(f"{which+1}.) {line.strip()}")
+        except Exception as ex:
+            raise ex
+
 
 if __name__ == '__main__':
-    import sys
-    sys.argv.append("-c")
-    sys.argv.append("This is a test message!")
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--create",
                         type=EzLog.Create,
                         help="Log message")
-    args = parser.parse_args()
+    parser.add_argument("-l", "--list",
+                        nargs=1,
+                        type=EzLog.List,
+                        help="List recent messages")
+    if os.path.exists(EzLog.LOGFILE):
+        os.unlink(EzLog.LOGFILE)
+    parser.parse_args(["-c", "This One."])
+    parser.parse_args(["-c", "This Two."])
+    parser.parse_args(["-c", "This Three."])
+    parser.parse_args(["-c", "This Four."])
+    parser.parse_args(["-l", "1"])
+    parser.parse_args(["-l", "11"])
+    parser.parse_args(["-l", "0"])
+    parser.parse_args(["-l", "-1"])
